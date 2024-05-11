8fc8001572f32373d13b28acaeb77f6743aed2b6
Aditya Dixit <53076095+Adx993@users.noreply.github.com.>
2024-05-11 21:47:14 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+ <?php
+ error_reporting('0');
+ require_once 'con.php';
+
+ $req_type = $_SERVER['REQUEST_METHOD'];
+
+ if ($_SESSION['logged_user_status'] == 1) {
+  
+   switch ($req_type) {
+      case 'POST':
+   
+         if(empty($_POST['view'])){
+   
+            include_once('layouts/login.php');
+        
+         }elseif ($_POST['view'] == 'login') {
+        
+            include_once('layouts/login.php');
+        
+         }elseif ( $_POST['view'] == 'dashboard') {
+        
+            include_once('layouts/dashboard.php');
+        
+         }
+   
+         break;
+   
+      case 'GET':
+   
+         if(empty($_GET['view'])){
+   
+            include_once('layouts/login.php');
+        
+         }elseif ($_GET['view'] == 'login') {
+   
+            include_once('layouts/login.php');
+        
+         }elseif($_GET['view'] == 'dashboard') {
+        
+            include_once('layouts/dashboard.php');
+        
+         }      
+   
+         break;
+      
+      default:
+         echo "<p>Invalid Request</p>: " . $req_type;
+         break;
+    }
+
+
+ }else{
+    include_once('layouts/login.php');
+ }
+
+
+
+ 
+ 
+  
+ 
+ ?>
